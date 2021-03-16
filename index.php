9f27165f09f64d3b6341c04717435df1255bb0b1
melvinsibichen <mca529@rajagiri.edu>
2021-03-16 11:12:49 -0700

    logo added

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-warning">
     <div class="container-fluid">
+      <img src="photos\logo.png" height="40" alt="">
       <a class="navbar-brand" href="#">MARIAN COLLEGE KUTTIKANAM</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
